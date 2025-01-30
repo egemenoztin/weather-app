dc2cea38240ecc392558c5b6d5746cedb8c08b80
Egemen Öztin <xaron.egemen.oztin@gmail.com>
2025-01-30 16:16:14 +0100

    Clear unnecessary comments

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -6,17 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class WeatherService {
-  private baseUrl: string = 'https://localhost:7171/api/weather/';  // Your backend API URL
+  private baseUrl: string = 'https://localhost:7171/api/weather/';
 
   constructor(private http: HttpClient) { }
 
-  // Updated getWeatherData to accept both city and unit
   getWeatherData(city: string, unit: string): Observable<any> {
     const url = `${this.baseUrl}getWeatherData?city=${city}&unit=${unit}`;
-    return this.http.get<any>(url);  // Make GET request with city and unit parameters
+    return this.http.get<any>(url);
   }
 
-  // Set temperature unit (metric or imperial)
   setTemperatureUnit(unit: string): Observable<any> {
     return this.http.post(`${this.baseUrl}setTemperatureUnit`, { unit });
   }
